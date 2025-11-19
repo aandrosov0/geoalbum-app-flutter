7308d3e8d7288a1de6381e1a3a5f060a9f36ba3b
Anton Androsov <aandrosov.kwork@gmail.com>
2025-11-19 14:04:39 +0300

    feat: add image editing feature

--- lib/ui/routes/routes.dart
+++ lib/ui/routes/routes.dart
@@ -5,8 +5,6 @@ import 'package:app/ui/screens/home_screen.dart';
 import 'package:app/ui/screens/photo_view_screen.dart';
 import 'package:app/ui/screens/photos_map_screen.dart';
 
-import '../screens/photo_edit_screen.dart';
-
 class HomeRoute {
   const HomeRoute();
 }
@@ -23,16 +21,9 @@ class PhotosMapRoute {
   const PhotosMapRoute({this.initialLocation});
 }
 
-class PhotoEditRoute {
-  final String photoPath;
-
-  const PhotoEditRoute({required this.photoPath});
-}
-
 Map<String, WidgetBuilder> get routes => {
   '/': (_) => const HomeScreen(),
   '$HomeRoute': (_) => const HomeScreen(),
   '$PhotoViewRoute': (_) => const PhotoViewScreen(),
   '$PhotosMapRoute': (_) => const PhotosMapScreen(),
-  '$PhotoEditRoute': (_) => const PhotoEditScreen(),
 };
