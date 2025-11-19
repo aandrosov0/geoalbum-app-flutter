622355c8219a54a7274a872f60b3d9538a964f17
Anton Androsov <aandrosov.kwork@gmail.com>
2025-11-19 14:02:37 +0300

    feat: add image editing feature

--- lib/ui/screens/photo_view_screen.dart
+++ lib/ui/screens/photo_view_screen.dart
@@ -5,6 +5,8 @@ import 'package:app/ui/widgets/plank.dart';
 import 'package:app/utils/files.dart';
 import 'package:app/utils/photos.dart';
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_cropper_aurora/image_cropper_aurora.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:path/path.dart' as path;
 import 'package:photo_view/photo_view.dart';
@@ -18,7 +20,10 @@ class PhotoViewScreen extends StatefulWidget {
 
 class _PhotoViewScreenState extends State<PhotoViewScreen> {
   late final NavigatorState _navigatorState;
+
   late final TextEditingController _fileNameController;
+  late final TextEditingController _photoWidthController;
+  late final TextEditingController _photoHeightController;
 
   LatLng? _photoLocation;
 
@@ -34,11 +39,15 @@ class _PhotoViewScreenState extends State<PhotoViewScreen> {
 
     _navigatorState = Navigator.of(context);
     _fileNameController = TextEditingController();
+    _photoWidthController = TextEditingController();
+    _photoHeightController = TextEditingController();
   }
 
   @override
   void dispose() {
     _fileNameController.dispose();
+    _photoWidthController.dispose();
+    _photoHeightController.dispose();
 
     super.dispose();
   }
@@ -50,6 +59,42 @@ class _PhotoViewScreenState extends State<PhotoViewScreen> {
     _arguments = ModalRoute.settingsOf(context)!.arguments as PhotoViewRoute;
     _photo = File(_arguments!.photoPath);
     _loadLocation();
+  }
+
+  Future<void> _cropImage(String filePath) async {
+    if (filePath.isEmpty) return;
+    try {
+      CroppedFile? croppedFile = await ImageCropper().cropImage(
+        sourcePath: filePath,
+        uiSettings: [
+          AuroraUiSettings(
+            context: context,
+            hasRightRotation: true,
+            hasLeftRotation: true,
+            gridColor: Colors.black,
+            scrimColor: Colors.black,
+            gridInnerColor: Colors.red,
+            gridCornerColor: Colors.amber,
+            cropButtonText: const Text('Применить изменения'),
+          ),
+        ],
+      );
+
+      if (croppedFile != null) {
+        final newPath = croppedFile.path;
+        _photo = File(newPath);
+
+        setState(() {});
+        if (!mounted) return;
+      }
+    } catch (e, stackTrace) {
+      debugPrint('Error cropping image: $e\n$stackTrace');
+      if (mounted) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text('Error cropping image: $e')));
+      }
+    }
   }
 
   @override
@@ -65,6 +110,14 @@ class _PhotoViewScreenState extends State<PhotoViewScreen> {
                     onTap: _showFileRenamingDialog,
                     child: const Text('Переименовать'),
                   ),
+                  PopupMenuItem(
+                    onTap: () => _cropImage(_photo?.path ?? ''),
+                    child: const Text('Редактировать'),
+                  ),
+                  PopupMenuItem(
+                    onTap: _onShare,
+                    child: const Text('Поделиться'),
+                  ),
                 ],
           ),
         ],
@@ -76,7 +129,7 @@ class _PhotoViewScreenState extends State<PhotoViewScreen> {
             Plank('Геометка отсутствует.\nИзображения не будет на карте.'),
           Expanded(
             flex: 1,
-            child: PhotoView(imageProvider: FileImage(_photo!)),
+            child: PhotoView(imageProvider: FileImage(_photo ?? File(''))),
           ),
         ],
       ),
@@ -109,8 +162,15 @@ class _PhotoViewScreenState extends State<PhotoViewScreen> {
     _photo = await _photo?.renameIgnoringExtension(name);
   }
 
+  void _onShare() {
+    final path = _photo?.path ?? '';
+    final uri = Uri.parse('file:$path');
+  }
+
   Future<void> _showFileRenamingDialog() {
-    _fileNameController.text = path.basenameWithoutExtension(_photo?.path ?? '');
+    _fileNameController.text = path.basenameWithoutExtension(
+      _photo?.path ?? '',
+    );
 
     return showDialog(
       context: context,
